a46b0ee7292c4602e9daca0e24e70a5d6bcd8aea
Elisabeth Palander <epalander@gmail.com>
2020-06-07 22:52:27 +0200

    a new flat can be added

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -12,11 +12,22 @@ class FlatsController < ApplicationController
     @flat = Flat.new
   end
 
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
 
 private
 
-   def set_flat
+  def set_flat
     @flat = Flat.find(params[:id])
   end
 
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture)
+  end
 end
