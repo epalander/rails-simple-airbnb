d670420394a7acef5e3d6c0c6b9c3e691d7455cd
Elisabeth Palander <epalander@gmail.com>
2020-06-07 21:23:11 +0200

    added a read more bottom for each flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,7 +1,16 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
     @flats = Flat.all
+  end
+
+  def show
+  end
+
+private
+   def set_flat
+    @flat = Flat.find(params[:id])
   end
 
 end
